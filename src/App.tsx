9e737a7098211f096eae6e078145e43d61dda084
Maswi, MR <rmaswi360@outlook.com>
2024-08-05 10:17:59 +0300

    Added bootstrap for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import ListGroup from "./ListGroup";
 import Message from "./Message";
 
 function App() {
   return (
     <div>
       <Message />
+      <ListGroup />
     </div>
   );
 }
---
+++ src/ListGroup.tsx
@@ -0,0 +1,26 @@
+function ListGroup() {
+  const cities = ["Dar", "Morogoro", "Dodoma", "Iringa"];
+  let selectedIndex = 0;
+  return (
+    <ul className="list-group" key="list">
+      <h2>List</h2>
+      {cities.map((city, index) => (
+        <li
+          className={
+            selectedIndex == index
+              ? "list-group-item active "
+              : "list-group-item"
+          }
+          onClick={() => {
+            console.log(city);
+            selectedIndex = index;
+          }}
+          key={city}
+        >
+          {city}
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default ListGroup;
